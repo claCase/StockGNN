ef092427e6d1185a42ddfb88ca3bfe85b40d8f84
Claudio_Casellato <claudiocasellatoml@gmail.com>
2023-04-06 17:56:04 +0200

    train tf function with profiler

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ from src import data
 import argparse
 import os
 import pickle as pkl
+from src.utils import train
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpu", action="store_true")
-    parser.add_argument("--epochs", default=500)
+    parser.add_argument("--epochs", default=500, type=int)
     parser.add_argument("--profile", action="store_true")
+    parser.add_argument("--save", action="store_true")
+    parser.add_argument("--show", action="store_true")
     args = parser.parse_args()
     gpu = args.gpu
     profile = args.profile
     epochs = args.epochs
+    save = args.save
+    show = args.show
 
     tickers_path = os.path.join(os.getcwd(), "data", "Tickers")
     if gpu:
@@ -54,72 +59,53 @@ if __name__ == "__main__":
     model.compile(optimizer=tf.keras.optimizers.Adam(0.001))  # ,loss=custom_mse,metrics=[custom_mse])
 
     a = np.ones(shape=(*diff_matrix.shape[:-1], diff_matrix.shape[-2]))
-    optim = tf.keras.optimizers.Adam(0.001)
     loss_hist = []
     t0 = 0
-    t1 = 40
+    t1 = 70
     dt = 25
 
     profiler_dir = os.path.join(os.getcwd(), "Profiler")
     options = tf.profiler.experimental.ProfilerOptions(host_tracer_level=3,
                                                        python_tracer_level=1,
                                                        device_tracer_level=1)
-
-
-    def train(model, x, y, epochs=500, log_dir="./", profiler_options=None):
-        @tf.function
-        def step(x, y):
-            with tf.GradientTape() as tape:
-                # with tf.profiler.experimental.Trace("inference", step_num=i):
-                o, p = model(x)
-                l = tf.reduce_mean(
-                    tf.keras.losses.mse(tf.reshape(y, (-1, 1)), tf.reshape(p, (-1, 1))))
-            # with tf.profiler.experimental.Trace("train", step_num=i):
-            grads = tape.gradient(l, model.trainable_variables)
-            optim.apply_gradients(zip(grads, model.trainable_variables))
-            return l
-
-        # tf.profiler.experimental.start(log_dir, options=profiler_options)
-        for i in range(epochs):
-            l = step(x, y)
-            loss_hist.append(l)
-            tf.print(f"Epoch {i}: {l}")
-        # tf.profiler.experimental.stop()
-
-
-    train(model,
-          x=[tf.constant(diff_matrix[t0:t1][None, :]), tf.constant(a[t0:t1][None, :])],
-          y=tf.constant(diff_matrix[t0 + 1:t1 + 1, :, -2]),
-          log_dir=profiler_dir,
-          profiler_options=options)
+    loss_hist = train(model,
+                      x=[tf.constant(diff_matrix[t0:t1][None, :]), tf.constant(a[t0:t1][None, :])],
+                      y=tf.constant(diff_matrix[t0 + 1:t1 + 1, :, -2]),
+                      epochs=epochs,
+                      log_dir=profiler_dir,
+                      profiler_options=options)
 
     o, p = model.predict([tf.constant(diff_matrix[t0:t1][None, :]),
                           tf.constant(a[t0:t1][None, :])]
                          )
-    plt.figure()
-    plt.plot(np.log(loss_hist))
-    plt.suptitle("Loss History")
-    plt.xlabel("Epochs")
-    plt.ylabel("MSE")
 
-    fig, ax = plt.subplots(10, 10)
-    fig.suptitle("Predictions On TRAINING SET (Blue) vs True (Red)")
-    cntr = 0
-    for ir in range(10):
-        for jc in range(10):
-            ax[ir, jc].plot(diff_matrix[t0:t1, cntr, -2], color="red")
-            ax[ir, jc].plot(p[0, :, cntr].flatten(), color="blue")
-            cntr += 1
+    if show:
+        plt.figure()
+        plt.plot(np.log(loss_hist))
+        plt.suptitle("Loss History")
+        plt.xlabel("Epochs")
+        plt.ylabel("MSE")
 
-    o_test, p_test = model.predict([tf.constant(diff_matrix[t0 + dt:t1 + dt][None, :]),
-                                    tf.constant(a[t0 + dt:t1 + dt][None, :])]
-                                   )
-    fig, ax = plt.subplots(10, 10)
-    fig.suptitle("Predictions On TEST SET (Blue) vs True (Red)")
-    cntr = 0
-    for ir in range(10):
-        for jc in range(10):
-            ax[ir, jc].plot(diff_matrix[t0 + dt + 1:t1 + dt + 1, cntr, -2], color="red")
-            ax[ir, jc].plot(p_test[0, :, cntr].flatten(), color="blue")
-            cntr += 1
-    plt.show()
+        fig, ax = plt.subplots(10, 10)
+        fig.suptitle("Predictions On TRAINING SET", fontsize=30)
+        cntr = 0
+        for ir in range(10):
+            for jc in range(10):
+                ax[ir, jc].plot(diff_matrix[t0 + 1:t1 + 1, cntr, -2], color="red", label="True")
+                ax[ir, jc].plot(p[0, :, cntr].flatten(), color="blue", label="Predicted")
+                ax[ir, jc].legend(loc="best")
+                cntr += 1
+
+        o_test, p_test = model.predict([tf.constant(diff_matrix[t0 + dt:t1 + dt][None, :]),
+                                        tf.constant(a[t0 + dt:t1 + dt][None, :])]
+                                       )
+        fig, ax = plt.subplots(10, 10)
+        fig.suptitle("Predictions On TEST SET", fontsize=30)
+        cntr = 0
+        for ir in range(10):
+            for jc in range(10):
+                ax[ir, jc].plot(diff_matrix[t0 + dt + 1:t1 + dt + 1, cntr, -2], color="red", label="True")
+                ax[ir, jc].plot(p_test[0, :, cntr].flatten(), color="blue", label="Predicted")
+                ax[ir, jc].legend(loc="best")
+                cntr += 1
+        plt.show()
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,39 @@
 import numpy as np
 import tensorflow as tf
+import os
+import datetime
 
+
+def train(model, x, y, epochs=500, log_dir="./", profiler_options=None):
+    if not model._compile_was_called:
+        optim = tf.keras.optimizers.Adam(0.001)
+
+    @tf.function
+    def step(x, y):
+        with tf.GradientTape() as tape:
+            with tf.profiler.experimental.Trace("inference", step_num=i):
+                o, p = model(x)
+            with tf.profiler.experimental.Trace("loss", step_num=i):
+                l = tf.reduce_mean(
+                    tf.keras.losses.mse(tf.reshape(y, (-1, 1)), tf.reshape(p, (-1, 1))))
+        with tf.profiler.experimental.Trace("train", step_num=i):
+            grads = tape.gradient(l, model.trainable_variables)
+            if model._compile_was_called:
+                model.optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            else:
+                optim.apply_gradients(zip(grads, model.trainable_variables))
+        return l
+
+    loss_hist = []
+    train_writer = tf.summary.create_file_writer(log_dir, "History")#os.path.join(log_dir, "History", str(datetime.datetime.now())[:-7]))
+    tf.profiler.experimental.start(log_dir,#os.path.join(log_dir, str(datetime.datetime.now())[:-7]),
+                                   options=profiler_options)
+    for i in range(epochs):
+        l = step(x, y)
+        loss_hist.append(l)
+        with train_writer.as_default(step=i):
+            tf.summary.scalar("MeanSquaredError", l)
+        tf.print(f"Epoch {i}: {l}")
+        if i == 30:
+            tf.profiler.experimental.stop()
+    return loss_hist
